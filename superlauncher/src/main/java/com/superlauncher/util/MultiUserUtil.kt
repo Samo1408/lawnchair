db4aa36fe2da748f87750af5f022ebc20bdd0201
Samo1408 <ab.0ss0mm@gmail.com>
2026-07-09 07:23:27 +0300

    feat: MultiUserUtil - install apps to other users

---
+++ superlauncher/src/main/java/com/superlauncher/util/MultiUserUtil.kt
@@ -0,0 +1,11 @@
+package com.superlauncher.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Process
+import android.os.UserHandle
+import android.provider.Settings
+import android.widget.Toast
+
+[... truncated for brevity ]
